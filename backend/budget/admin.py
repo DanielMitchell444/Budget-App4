ab9aa80da19a72237009f91401ef6c50e58b2056
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2024-12-30 14:08:51 -0800

    Delete backend directory

--- backend/budget/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import Users
-# Register your models here.
-
-@admin.register(Users)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['email', 'first_name', 'last_name', 'username', 'is_active', 'is_staff']
-    ordering = ['email']
